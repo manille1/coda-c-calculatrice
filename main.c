3c617e6cf6924404cb7bbaddcf5759833b25d8b9
manille <manilledesseaux@gmail.com>
2024-09-26 16:35:33 +0200

    etape 3 : recupérer les infos

--- main.c
+++ main.c
@@ -6,4 +6,13 @@ int main()
 	int nb_1;
 	int nb_2;
 	char operateurs;
+
+	printf("Bonjour, choisissez un opérateurs parmis : +, -, *, /, %% :\n");
+	scanf("%s", operateurs);
+
+	printf("Merci, maintenant choisissez le premier nombre de votre opération.\n");
+	scanf("%d", &nb_1);
+
+	printf("Maintenant saisissez votre second nombre.\n");
+	scanf("%d", &nb_2)
 }
